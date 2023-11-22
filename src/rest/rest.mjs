10ce9c7b4233a2f8af4fc7500ab9066c1b76a2ca
jobbol <102873247+jobbol@users.noreply.github.com>
2023-11-21 22:21:24 -0600

    Routes hooked up to express

--- src/rest/rest.mjs
+++ src/rest/rest.mjs
@@ -1,7 +1,8 @@
 import express from 'express';
-import contentType from 'content-type';
-import getRawBody  from 'raw-body';
-import bodyParser from 'body-parser';
+import cors from 'cors';
+import { getGlobals } from 'common-es'
+const { __dirname, __filename } = getGlobals(import.meta.url);
+
 
 import folderImport from '../utils/folder-import.mjs';
 import configGet  from '../utils/config-get.mjs';
@@ -10,41 +11,27 @@ const { restPort } = configGet("./config/server.json", {restPort: 8101});
 const app = express();
 
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
-app.use(bodyParser.json());
-
-//Limit max request bodies to prevent DoS.
-app.use(function (req, res, next) {
-    getRawBody(req, {
-      length: req.headers['content-length'],
-      limit: '1kb',
-      encoding: contentType.parse(req).parameters.charset
-    }, function (err, string) {
-      if (err) return next(err)
-      req.text = string
-      next()
-    })
-});
-
-
 //Limit max request bodies to prevent DoS.
 app.use(express.urlencoded({ extended: true, limit: "1kb" }));
 app.use(express.json({ limit: "1kb" }));
 
+app.use(cors());
+
+app.use((req, res, next) => {
+  console.log(req.originalUrl);
+  next();
+});
+
+app.listen(restPort);
 
 
 
-export default function ({global}) {
-    app.get('/', (req, res) => {
-        res.send('hello world');
-    });
-    app.listen(restPort)
+//Initialize all routes then pass app and global to them.
+export default async function ({global}) {
+  const routes = await folderImport(__dirname + '/routes');
+  routes.map(route => route.default({app, global}));
 }
 
-const routes = folderImport('./rest/routes');
-console.log(routes);
 
 
 //Prevent errors from bubbling up to user.
@@ -52,4 +39,4 @@ console.log(routes);
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('{error: An error has occured.}');
- });
+});
--- src/utils/generate-id.mjs
+++ src/utils/generate-id.mjs
@@ -4,7 +4,7 @@ import { generateSlug  } from "random-word-slugs";
  * Uses generateSlug to create new adjective-adjective-noun slug IDs, and avoids collision with existing IDs.  
  * @param {object} global - The global object passed around the whole program.
  * @param {object[]} global.users - Array of user objects.
- * @param {object[]} global.games - Array of game objects.
+ * @param {object[]} global.rooms - Array of room objects.
  * @returns {string} Either a slug ID or the current UNIX timestamp as a fallback.
  * @example 
  * export default function ({global}) {
@@ -13,12 +13,13 @@ import { generateSlug  } from "random-word-slugs";
  */
 export default function generateID (global) {
     let out;
-    for(let i = 0, collision = true; collision; i++) {
+    let collision = true;
+    for(let i = 0; collision; i++) {
 
         //Try to get a unique slug type.
         out = generateSlug();
         collision = global.users.some(user => user.id === out) ||
-            global.games.some(game => game.id === out);
+            global.rooms.some(room => room.id === out);
 
         //If we loop too many times, fallback to Date.now() as an ID.
         if (i > 10) {
